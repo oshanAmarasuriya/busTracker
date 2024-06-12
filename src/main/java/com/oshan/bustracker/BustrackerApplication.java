e4e0fabf88a24c02a01295242a407e43207af02d
oshanAmarasuriya <oshansandeep630@gmail.com>
2024-06-13 00:19:38 +0530

    Initial commit

---
+++ src/main/java/com/oshan/bustracker/BustrackerApplication.java
@@ -0,0 +1,13 @@
+package com.oshan.bustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BustrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BustrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oshan/bustracker/BustrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oshan.bustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BustrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
